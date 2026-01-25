336b5c944272e2f7844740353da77ab041f86463
happer64bit <happer64bit@gmail.com>
2026-01-25 12:29:26 +0630

    build: migrate from tailwindcss v3 to v4

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), tanstackRouter({
+  plugins: [tailwindcss(), react(), tanstackRouter({
     target: "react",
     autoCodeSplitting: true
   })],
